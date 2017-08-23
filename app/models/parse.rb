c2afe89bf07ef663fc602262c76f6843bf272e4c
richardtylee <richardtylee@gmail.com>
2017-08-22 21:42:14 -0700

    Refactor Parse module.

--- app/models/parse.rb
+++ app/models/parse.rb
@@ -1,33 +1,36 @@
 class Parse
-  def self.parse(score)
-    balls = score.chars
-    converted_array = []
+	class << self
+	  def parse(data)
+	    balls = data.chars
+	    converted_array = []
 
-    for i in 0..balls.length-1
-      converted = convert(balls[i])
-      if converted == "/"
-      	converted = spare(balls[i-1], converted)
-      end
-      converted_array << converted
-    end
+	    for i in 0..balls.length-1
+	      converted = convert(balls[i])
+	      if converted == "/"
+	      	converted = spare(balls[i-1], converted)
+	      end
+	      converted_array << converted
+	    end
 
-    converted_array
-  end
+	    converted_array
+	  end
 
-  def self.convert(frame)
-  	if frame == "X"
-  	  10
-  	elsif frame == "-"
-  	  0
-  	elsif frame == "/"
-  	  frame
-  	else
-  	  frame.to_i
-  	end
-  end
+	  def convert(ball)
+	  	case ball
+		  	when "X"
+		  	  10
+		  	when "-"
+		  	  0
+		  	when "/"
+		  	  "/"
+		  	else
+		  	  ball.to_i
+	  	end
+	  end
 
-  def self.spare(previous, current)
-  	current = 10 - convert(previous)
-  	current
-  end
+	  def spare(previous, current)
+	  	current = 10 - convert(previous)
+	  	current
+	  end
+	end
 end
